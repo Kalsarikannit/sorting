3f3ca4cc2d6bfd6c0f2a18069d5891d017511846
Andrew Hookom <hookom@gmail.com>
2017-03-02 12:10:01 -0500

    "bubbleSort done"

--- bubblesort.js
+++ bubblesort.js
@@ -1,1 +1,22 @@
+function swap(a, b){
+  const buf = a;
+  a = b;
+  b = buf;
+}
 
+function bubbleSort(input){
+  let counter = 0;
+
+  for (let j = 0;j < input.length;j++){
+    for (let i = 0;i < input.length - 1 - j;i++){
+      if (input[i] > input[i + 1]){
+        //console.log(input);
+        const buf = input[i];
+        input[i] = input[i + 1];
+        input[i + 1] = buf;
+        console.log(input);
+      }
+    }
+  }
+  return input;
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,1 +1,12 @@
 
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+  it("handles random array",function(){
+    expect( bubbleSort([5,2,9,1])).toEqual([1,2,5,9]);
+  });
+  it("handles non-numbers",function(){
+    expect(bubbleSort(['z','b']).toEqual(['b','z']));
+  });
+});
