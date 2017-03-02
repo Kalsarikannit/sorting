de79c94fe82c16aa74b9c53eb3e3e0dd5c087e4d
Kalsarikannit <kenwoo0726@gmail.com>
2017-03-02 12:40:01 -0700

    a

--- mergesort.js
+++ mergesort.js
@@ -1,1 +1,34 @@
 
+function mergeSort(input){
+
+  if(input.length < 2){
+    return input;
+  }else{
+    let index = Math.floor(input.length / 2);
+    let left = mergeSort(input.slice(0,index));
+    let right = mergeSort(input.slice(index));
+    return merge(left, right);
+  }
+
+}
+
+function merge(left, right){
+  let arr = [];
+  while(left.length > 0 && right.length > 0){
+    console.log('left',left);
+    console.log('right',right);
+    console.log('arr',arr);
+    if(left[0] < right[0]){
+      arr.push(left.shift());
+    }else{
+      arr.push(right.shift());
+    }
+  }
+  if(left.length === 0){
+    arr = arr.concat(right);
+  }else{
+    arr = arr.concat(left);
+  }
+  console.log('return arr', arr);
+  return arr;
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,1 +1,12 @@
 
+describe('Merge Sort', function(){
+  it('handles an empty array', function(){
+    expect( mergeSort([]) ).toEqual( [] );
+  });
+  it("handles random array",function(){
+    expect( mergeSort([5,2,9,1])).toEqual([1,2,5,9]);
+  });
+  it("handles non-numbers",function(){
+    expect(mergeSort(['z','b']).toEqual(['b','z']));
+  });
+});
